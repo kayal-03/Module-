721ec32b13032e2d9546db52224fa7107c9bbbec
kayal-03 <kayalvarshini07@gmail.com>
2026-07-30 19:14:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,24 +1,23 @@
+"""
+URL configuration for studentproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from . import views
-
-router = DefaultRouter()
-router.register("students", views.StudentViewSet)
-router.register("faculty", views.FacultyViewSet)
-router.register("admins", views.AdminProfileViewSet)
-router.register("marks", views.MarksViewSet)
-router.register("attendance", views.AttendanceViewSet)
-router.register("assignments", views.AssignmentScoreViewSet)
-router.register("predictions", views.PerformancePredictionViewSet)
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
-    path("api/dashboard/", views.dashboard, name="api-dashboard"),
-    path("api/register/", views.register, name="api-register"),
-    path("api/login/", views.login, name="api-login"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
-    path("api/", include(router.urls)),
+    path('', include('students.urls')),
+    path('admin/', admin.site.urls),
 ]
